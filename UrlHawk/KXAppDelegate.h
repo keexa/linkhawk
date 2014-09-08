4c79e0074b0849d3809d6b37289bc05d6f7b359d
Marco Bonifazi <marco.bonifazi@anteleon.com>
2014-09-08 17:32:35 +0100

    Initial Commit

---
+++ UrlHawk/KXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KXAppDelegate.h
+//  UrlHawk
+//
+//  Created by Marco Bonifazi on 08/09/2014.
+//  Copyright (c) 2014 Keexa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
